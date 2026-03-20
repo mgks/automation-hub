fe7c95ac06905df81fffaafcc072fd9bbc105061
Ghazi <hello@mgks.dev>
2026-03-20 07:24:28 +0530

    Update static

--- static/app.js
+++ static/app.js
@@ -106,12 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <p class="item-description">${trimmedDescription}</p>
                         <div class="pills-container">${toolPill}${tagsHTML}</div>
                     </div>
-                    <div class="item-actions">
-                        <a href="${item.downloadUrl}" class="icon-button" download target="_blank" title="Download JSON">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="theme-icon lucide lucide-arrow-down-to-line-icon lucide-arrow-down-to-line"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
-                        </a>
-                    </div>
-                </div>`;
+                    </div>`;
         }).join('');
     }
 
